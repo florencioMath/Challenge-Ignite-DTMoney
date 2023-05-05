a4efae43330d00b9da63e8beb6694af06d6e0068
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-05-05 18:48:13 -0300

    fix: Removing unnecessary code.

--- dt-money/src/pages/Transactions/index.tsx
+++ dt-money/src/pages/Transactions/index.tsx
@@ -1,13 +1,7 @@
 import { Header } from '../../components/Header';
 import { Summary } from '../../components/Summary';
-import {
-  PriceHighlight,
-  TransactionAmount,
-  TransactionsContainer,
-  TransactionsTable,
-} from './styles';
+import { TransactionAmount, TransactionsContainer } from './styles';
 import { useContextSelector } from 'use-context-selector';
-import { dateFormatter, priceFormatter } from '../../utils/formatter';
 import { TransactionsContext } from '../../contexts/TransactionsContext';
 import { CardTransactions } from './components/CardTransaction';
 import { SearchForm } from './components/SearchForm/SearchForm';
@@ -37,27 +31,6 @@ export function Transactions() {
             </div>
           );
         })}
-
-        {/* <TransactionsTable>
-          <tbody>
-            {transactions.map((transaction) => {
-              return (
-                <tr key={transaction.id}>
-                  <td width="50%">{transaction.description}</td>
-                  <td>
-                    <PriceHighlight variant={transaction.type}>
-                      {priceFormatter.format(transaction.price)}
-                    </PriceHighlight>
-                  </td>
-                  <td>{transaction.category}</td>
-                  <td>
-                    {dateFormatter.format(new Date(transaction.createdAt))}
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </TransactionsTable> */}
       </TransactionsContainer>
     </div>
   );
